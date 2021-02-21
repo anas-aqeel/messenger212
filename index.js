813378f9c5e82a013ec7b9fb5be00b578dc8b610
Anas-Aqeel <71166989+Anas-Aqeel@users.noreply.github.com>
2021-02-21 19:28:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
-const port = PROCESS.ENV.PORT || 8000
+const port = process.env.PORT || 8000
 const io = require('socket.io')(http)
 
 const users = []
